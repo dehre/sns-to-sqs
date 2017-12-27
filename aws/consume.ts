472a09d518e5dd4ce1775ccceea4eed2d7bc4e17
L-oris <loris4526@gmail.com>
2017-12-27 21:21:24 +0100

    Consume and delete messages from poll. Still not working properly

---
+++ aws/consume.ts
@@ -0,0 +1,28 @@
+import { sqs } from './initialize'
+import { SQS } from 'aws-sdk';
+
+// get the messages and clear the pool
+export async function consumeMessages(queueUrl: string) {
+  const receiveParams: SQS.ReceiveMessageRequest = {
+    QueueUrl: queueUrl,
+    MaxNumberOfMessages: 10
+  }
+
+  const data: SQS.ReceiveMessageResult = await sqs.receiveMessage(receiveParams).promise()
+  if(!(data && data.Messages && data.Messages.length > 0)) return false
+
+  // do something with messages
+  data.Messages.forEach((message: SQS.Message) => {
+    console.log('message -->')
+    console.log(message.Body)
+
+    // Delete the message when we've successfully processed it
+    var deleteParams = {
+      QueueUrl: queueUrl,
+      ReceiptHandle: message.ReceiptHandle
+    }
+    sqs.deleteMessage(deleteParams, () => {})
+  })
+
+  return true
+}
