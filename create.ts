39a708118c13a78b780cf51dd72ebfae6e1a9622
L-oris <loris4526@gmail.com>
2017-12-27 17:39:45 +0100

    Try to update permissions for SQS, failed to set correct attributes

--- create.ts
+++ create.ts
@@ -16,8 +16,8 @@ const configurationOptions: ConfigurationOptions = {
 AWS.config.update(configurationOptions)
 
 
-const sns: SNS = new AWS.SNS()
-const sqs: SQS = new AWS.SQS()
+const sns: SNS = new AWS.SNS({apiVersion: '2010-03-31'})
+const sqs: SQS = new AWS.SQS({apiVersion: '2012-11-05'})
 
 
 // create SNS topic by given a topic name and return ARN
@@ -32,15 +32,22 @@ export async function createSNSTopic(topicName: string): Promise<string | null> 
 }
 
 
-// create SQS queue by given topic name and return ARN
+// create SQS queue by given topic name and returns queue Url
 export async function createSQSQueue(queueName: string): Promise<string | null> {
   const queueParams: SQS.CreateQueueRequest = {
     QueueName: queueName
   }
   const queueResult: SQS.CreateQueueResult  = await sqs.createQueue(queueParams).promise()
 
+  if(!queueResult.QueueUrl) return null
+  return queueResult.QueueUrl
+}
+
+
+// receives queue url and return its ARN
+export async function getSQSArn(queueUrl: string): Promise<string | null> {
   const queueAttributesParams: SQS.GetQueueAttributesRequest = {
-    QueueUrl: queueResult.QueueUrl as string,
+    QueueUrl: queueUrl,
     AttributeNames: ['QueueArn']
   }
   const queueAttributesResult: SQS.GetQueueAttributesResult = await sqs.getQueueAttributes(queueAttributesParams).promise()
@@ -59,6 +66,20 @@ export async function subscribeSQStoSNS(snsTopicARN: string, sqsQueueARN: string
   }
   
   await sns.subscribe(subscribeParams).promise()
+}
+
+
+// add permissions to enable sns topic to write in sqs queue
+export async function updateSQSPermissions(snsTopicARN: string, sqsQueueUrl: string, sqsQueueARN: string): Promise<void> {
+  const attributes: SQS.QueueAttributeMap = {
+    QueueArn: sqsQueueARN
+  }
+  const params: SQS.SetQueueAttributesRequest = {
+    QueueUrl: sqsQueueUrl,
+    Attributes: attributes
+  }
+
+  await sqs.setQueueAttributes(params)
 }
 
 
--- router.ts
+++ router.ts
@@ -1,5 +1,5 @@
 import * as Router from 'koa-router'
-import { createSNSTopic, createSQSQueue } from './create'
+import { createSNSTopic, createSQSQueue, getSQSArn, subscribeSQStoSNS, updateSQSPermissions } from './create'
 
 const router: Router = new Router()
 
@@ -8,11 +8,18 @@ router.get('/', ctx => {
 })
 
 router.get('/create', async ctx => {
+  const sqsQueueUrl = await createSQSQueue('demo')
+  if(!sqsQueueUrl) throw new Error('Cannot set up SQS Queue')
+
+  const sqsQueueArn = await getSQSArn(sqsQueueUrl)
   const snsTopicArn = await createSNSTopic('demo')
-  const sqsQueueArn = await createSQSQueue('demo')
+  if(!(snsTopicArn && sqsQueueArn)) throw new Error('Cannot set up SNS Topic')
+
+  await subscribeSQStoSNS(snsTopicArn, sqsQueueArn)
+  await updateSQSPermissions(snsTopicArn, sqsQueueUrl, sqsQueueArn)
+
   ctx.body = {
-    snsTopicArn,
-    sqsQueueArn
+    message: 'setup is fine'
   }
 })
 
